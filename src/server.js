c965ab88c11bff2d8822241bc30f31a3a64564ae
Jashany <jsingh12_be22@thapar.edu>
2024-06-20 11:01:34 +0530

    cookie

--- src/server.js
+++ src/server.js
@@ -32,7 +32,9 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Origin, X-Requested-With, Content-Type, Accept',
+        'Set-Cookie', 
+        'SameSite=None; Secure',
     );
     next();
 });
