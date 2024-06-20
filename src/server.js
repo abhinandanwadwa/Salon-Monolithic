f2310bc5c39485ec3290ab872d381783e1b3de20
Jashany <jsingh12_be22@thapar.edu>
2024-06-20 11:09:26 +0530

    cookie fail  try 100304050

--- src/server.js
+++ src/server.js
@@ -30,12 +30,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://www.salondekho.in');
     res.header('Access-Control-Allow-Credentials', true);
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-        'Set-Cookie', 
-        'SameSite=None; Secure',
-    );
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
--- src/utils/generatetoken.js
+++ src/utils/generatetoken.js
@@ -11,7 +11,7 @@ const generateToken = (res, user) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: true,
-    SameSite: "None",
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
