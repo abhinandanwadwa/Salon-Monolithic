60781fcec7804dc9682df9b7466fb6f043b4427c
Jashany <jsingh12_be22@thapar.edu>
2024-06-20 08:24:40 +0530

    sameSite change

--- src/utils/generatetoken.js
+++ src/utils/generatetoken.js
@@ -11,7 +11,7 @@ const generateToken = (res, user) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
