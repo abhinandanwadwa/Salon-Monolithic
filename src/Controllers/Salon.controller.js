104d20d70c834162a80444e4391314c9e87b4aed
Jashany <jsingh12_be22@thapar.edu>
2024-06-28 18:48:28 +0530

    error push

--- src/Controllers/Salon.controller.js
+++ src/Controllers/Salon.controller.js
@@ -150,7 +150,7 @@ const createSalon = async (req, res) => {
     console.error(error);
     return res.status(500).json({
       success: false,
-      message: "Error in creating salon",
+      message: "Error in creating salon" + error,
     });
   }
 };
