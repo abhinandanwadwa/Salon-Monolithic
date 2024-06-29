99750422a0fc645b84f1e9024dd07187a3b2383d
Jashany <jsingh12_be22@thapar.edu>
2024-06-29 08:50:51 +0530

    db push

--- src/DB/index.js
+++ src/DB/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    const connection = await mongoose.connect(`mongodb+srv://notmeits11:notmeits11@salons.bqfujxq.mongodb.net/salons`);
+    const connection = await mongoose.connect(`mongodb+srv://jashanmaybe75:zJNuagx59eZ4Imk1@cluster0.uy4nu8u.mongodb.net/Salondekho`);
     console.log(`MongoDB connected : ${connection.connection.host}`);
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
