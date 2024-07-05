2b9cfba0cb50dfc2383fa4c80a00e0954fe08732
Abhinandan Wadhwa <74089487+abhinandanwadwa@users.noreply.github.com>
2024-07-05 10:13:31 +0530

    fix: database url connection

--- src/DB/index.js
+++ src/DB/index.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    const connection = await mongoose.connect(`${process.env.MONGO_URI}/Salondekho`);
+    const connection = await mongoose.connect(`${process.env.MONGO_URI}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'SalonDekho',
+    });
     console.log(`MongoDB connected : ${connection.connection.host}`);
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
