edf6dc97d89727038d4a96d3de24e0f478cc4156
Abhinandan Wadhwa <74089487+abhinandanwadwa@users.noreply.github.com>
2024-07-05 10:05:27 +0530

    fix: changed mongo uri

--- src/DB/index.js
+++ src/DB/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    const connection = await mongoose.connect(`mongodb+srv://jashanmaybe75:zJNuagx59eZ4Imk1@cluster0.uy4nu8u.mongodb.net/Salondekho`);
+    const connection = await mongoose.connect(`${process.env.MONGO_URI}/Salondekho`);
     console.log(`MongoDB connected : ${connection.connection.host}`);
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
