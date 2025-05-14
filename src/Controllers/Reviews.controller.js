c43ba7f7f67a0914b8d6cfe0a64c0080b45c7a38
Jashany <jsingh12_be22@thapar.edu>
2025-05-14 21:58:16 +0530

    Review Change

--- src/Controllers/Reviews.controller.js
+++ src/Controllers/Reviews.controller.js
@@ -23,34 +23,10 @@ const createReview = async (req, res) => {
             return res.status(400).json({ message: "Review already created" });
         }
 
-        const Artist = await ArtistModel.findById(appointment.artist);
         const Salon = await SalonModel.findById(appointment.salon);
 
-        const ArtistUser = await UserModel.findOne(Artist.userId);
-        const SalonOwner = await UserModel.findOne(Salon.userId);
 
-        let salonTokens = [];
-        let Ids = [];
-
-        for(let i = 0; i < Salon.subAdmins.length; i++){
-            const subAdmin = await UserModel.findOne(Salon.subAdmins[i]);
-            if(subAdmin.token){
-                salonTokens.push(subAdmin.token);
-                
-            }
-            Ids.push(subAdmin._id);
-        }
-
-        Ids.push(Salon.userId);
-        Ids.push(Artist.userId);
-
-        if(ArtistUser.token){
-            salonTokens.push(ArtistUser.token);
-        }
-
-        if(SalonOwner.token){
-            salonTokens.push(SalonOwner.token);
-        }
+       
 
         if (!user) {
             return res.status(404).json({ message: "User not found" });
@@ -70,51 +46,49 @@ const createReview = async (req, res) => {
 
 
 
-        Artist.reviews.push(NewReview._id);
         Salon.Reviews.push(NewReview._id);
 
-        await Artist.save();
         await Salon.save();
 
         appointment.Review = NewReview._id;
         await appointment.save();
 
-        salonTokens = [...new Set(salonTokens)];
+        // salonTokens = [...new Set(salonTokens)];
 
-        if(salonTokens.length > 0){
-            const message = {
-                notification: {
-                    title: 'New Review',
-                    body: `${name} gave you a rating`,
-                },
-                tokens: salonTokens
-            }
+        // if(salonTokens.length > 0){
+        //     const message = {
+        //         notification: {
+        //             title: 'New Review',
+        //             body: `${name} gave you a rating`,
+        //         },
+        //         tokens: salonTokens
+        //     }
     
-            messaging.sendEachForMulticast(message)
-            .then((response) => {
-                console.log('Successfully sent message:', response);
-            })
-            .catch((error) => {
-                console.log('Error sending message:', error);
-            });
-        }
+        //     messaging.sendEachForMulticast(message)
+        //     .then((response) => {
+        //         console.log('Successfully sent message:', response);
+        //     })
+        //     .catch((error) => {
+        //         console.log('Error sending message:', error);
+        //     });
+        // }
 
-        Ids = [...new Set(Ids)];
+        // Ids = [...new Set(Ids)];
 
-        const nameArtist = Artist.name || ArtistUser.name || "Artist";
+        // const nameArtist = Artist.name || ArtistUser.name || "Artist";
 
-        db.collection("Notification").add({
-            title: "New Review",
-            body: `${name} gave you a rating`,
-            Ids: Ids.map(id => id.toString()),
-            read: false,
-            related: nameArtist,
-            createdAt: new Date().toISOString(),
-          }).then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
-          }).catch((error) => {
-            console.error("Error adding document: ", error);
-          });
+        // db.collection("Notification").add({
+        //     title: "New Review",
+        //     body: `${name} gave you a rating`,
+        //     Ids: Ids.map(id => id.toString()),
+        //     read: false,
+        //     related: nameArtist,
+        //     createdAt: new Date().toISOString(),
+        //   }).then((docRef) => {
+        //     console.log("Document written with ID: ", docRef.id);
+        //   }).catch((error) => {
+        //     console.error("Error adding document: ", error);
+        //   });
         
         
         return res.status(201).json({ 
