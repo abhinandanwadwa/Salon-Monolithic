3ba530c26b49dd7ea2a7ab0d56feea9a0440b9e0
Jashany <jsingh12_be22@thapar.edu>
2025-05-19 09:16:38 +0530

    revert Back

--- src/Controllers/fcmClient.js
+++ src/Controllers/fcmClient.js
@@ -3,7 +3,7 @@ import { getMessaging } from "firebase-admin/messaging";
 import admin from "firebase-admin";
 import dotenv from "dotenv";
 dotenv.config();
-import serviceAccount from "../../salondekho-55d7c-firebase-adminsdk-s6yy5-3dd2ecc90f.json" assert { type: "json" };
+import serviceAccount from "../../salondekho-55d7c-firebase-adminsdk-s6yy5-768fa5b955.json" assert { type: "json" };
 
 const app = initializeApp({
     credential: admin.credential.cert(serviceAccount),
